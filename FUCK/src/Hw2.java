e06adc74cd0838a86fb0fa5d658a059c5d74ade9
upinblayze <zhenghannin@gmail.com>
2016-08-08 20:33:19 -0700

    New branch, cleaner

--- FUCK/src/Hw2.java
+++ FUCK/src/Hw2.java
@@ -2,8 +2,17 @@
 public class Hw2 {
 
 	public static void main(String[] args) {
-		
+
 		MultiArray hw= new MultiArray();
+		System.out.println();
+		hw.patternRow();
+		hw.sumArray();
+		System.out.println();
+		hw.patternCheckered();
+		hw.sumArray();
+		System.out.println();
+		hw.userChoice();
+		hw.sumArray();
 		
 	}
 
--- FUCK/src/MultiArray.java
+++ FUCK/src/MultiArray.java
@@ -1,24 +1,80 @@
+import java.util.Scanner;
+
 public class MultiArray
 {
 	private int[][] grid;
-	
+
 	public MultiArray(){
 		grid = new int[10][10];
-		for(int column = 0; column < 10 ; column++)
+		for(int col = 0; col < 10 ; col++)
 		{
-				
+
 			for(int row = 0; row < 10; row++)
 			{
-				grid[column][row] = 1;		
-				System.out.print(grid[column][row]);
+				grid[row][col] = 1;		
 			}
-			System.out.println("");
 		}
-		System.out.println("");	
+		printArray();
 	}
-		
-	public static void patternRow(){
-		
+
+	private void printArray(){
+		for(int[] row:grid){
+			for(int col:row){
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+	}
+	public void patternRow(){
+		for(int row = 0; row < 10 ; row++){
+			for(int col = 0; col < 10; col++)
+			{
+				grid[row][col] = row%2;		
+			}
+		}
+		printArray();
+	}
+
+	public void patternCheckered(){
+		for(int row = 0; row < 10 ; row++)
+		{
+			for(int col = 0; col < 10; col++)
+			{
+				grid[row][col] = (row+col)%2;;		
+			}
+		}
+		printArray();
+	}
+
+	public void userChoice(){
+		Scanner scan=new Scanner(System.in);
+		int[] pattern=new int[3];
+		for(int choices=0; choices<3; choices++){
+			System.out.print("Please enter your choice: ");
+			pattern[choices]=scan.nextInt();
+		}
+		scan.close();
+		for(int col = 0; col < 10 ; col++)
+		{
+			for(int row = 0; row < 10; row++)
+			{
+				grid[col][row] = pattern[(row+col)%3];		
+			}
+		}
+		printArray();
 	}
 	
+	public void sumArray(){
+		int total=0;
+		for(int row=0; row<10; row++){
+			int rowSum=0;
+			for(int col=0; col<10; col++){
+				rowSum+=grid[row][col];
+				total+=grid[row][col];
+			}
+			System.out.println("Total for row "+row+": " + rowSum);
+		}
+		System.out.println("Total sum:" +total);
+	}
+
 }
